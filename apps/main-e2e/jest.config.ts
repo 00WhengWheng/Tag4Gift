e5546055376a0de3a3ab09539b936a8627cbefab
00WhengWheng <francesco.michelagnoli@gmail.com>
2025-08-03 20:15:16 +0200

    modified:   apps/main-e2e/jest.config.ts 	modified:   apps/main-e2e/tsconfig.json 	modified:   apps/main-e2e/tsconfig.spec.json 	modified:   apps/main/tsconfig.app.json 	modified:   nx.json 	new file:   tsconfig.base.json

--- apps/main-e2e/jest.config.ts
+++ apps/main-e2e/jest.config.ts
@@ -1,15 +1,15 @@
 export default {
   displayName: 'main-e2e',
   preset: '../jest.preset.js',
-  globalSetup: '<rootDir>/src/support/global-setup.ts',
-  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
-  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  globalSetup: '<rootDir>/../main-e2e/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/../main-e2e/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/../main-e2e/src/support/test-setup.ts'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': [
       'ts-jest',
       {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
+        tsconfig: '<rootDir>/../main-e2e/tsconfig.spec.json',
       },
     ],
   },
